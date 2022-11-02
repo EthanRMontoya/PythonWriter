a014d29c3e9e30cc219b8c0301132dc9d87bb2c8
EthanRMontoya <ineeda240sx@gmail.com>
2022-11-01 19:17:29 -0600

    got it to change

--- noveskebot.py
+++ noveskebot.py
@@ -1,16 +1,32 @@
 from pynput.keyboard import Key, Controller
+from pynput.mouse import Button
+from pynput import mouse
+
 import time
 
 keyboard = Controller()
 fo = open("emails.txt", "r")
+delayTime = input("enter your delay time in seconds")
 while True:
         coolguy = input("y Enter to Cont, X to quit")
+        if coolguy == 'x':
+            delayTime = 0
+        time.sleep(int(delayTime))
         if coolguy == 'y':
             fo.seek(0,0)
             for line in fo.readlines():
-                delay = time.sleep(2)
                 keyboard.type(line)
                 keyboard.tap(Key.enter)
-                time.sleep(0.13)
+                keyboard.press(Key.cmd)
+                keyboard.press(Key.left)
+                time.sleep(3.0)
+                keyboard.release(Key.cmd)
+                keyboard.release(Key.left)
+                time.sleep(3.0)
+                keyboard.press(Key.cmd)
+                keyboard.press('a')
+                keyboard.tap(Key.backspace)
+                keyboard.release('a')
+                keyboard.release(Key.cmd)
         else:
             exit()
