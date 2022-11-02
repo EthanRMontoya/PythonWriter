edbeb1af242a66bec448cb3db1ae045b1f16d4f0
EthanRMontoya <ineeda240sx@gmail.com>
2022-11-01 18:37:49 -0600

    got it to go one by one

--- noveskebot.py
+++ noveskebot.py
@@ -2,11 +2,15 @@ from pynput.keyboard import Key, Controller
 import time
 
 keyboard = Controller()
+fo = open("emails.txt", "r")
 while True:
-    emails = input("enter emails ")
-    if emails == 'x':
-        break
-    delay = time.sleep(5)
-    keyboard.type(emails)
-    time.sleep(0.13)
-    keyboard.press(Key.enter)
+        coolguy = input("y Enter to Cont, X to quit")
+        if coolguy == 'y':
+            fo.seek(0,0)
+            for line in fo.readlines():
+                delay = time.sleep(2)
+                keyboard.type(line)
+                keyboard.tap(Key.enter)
+                time.sleep(0.13)
+        else:
+            exit()
